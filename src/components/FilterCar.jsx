9e2927aceb33eeb6b409c1949248189fa848bc65
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-04-05 17:39:51 +0200

    filterByAvaliability

--- src/components/FilterCar.jsx
+++ src/components/FilterCar.jsx
@@ -1,14 +1,53 @@
-const FilterCar = () => {
-  return (
-    <div>
-      <form className="search-form filter-car">
-        <input type="text" placeholder="Pick-up Location" />
-        <input type="date" placeholder="Pick-up Date" />
-        <input type="date" placeholder="Drop-off Date" />
-        <button type="submit">Search</button>
-      </form>
+import React, { useState } from 'react';
+import axios from 'axios';
+import CardProduct from './CardProduct' 
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+function FilterCar() {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [availableCars, setAvailableCars] = useState([]);
+
+    const fetchAvailableCars = () => {
+       
+        axios.get(`${API_URL}/api/cars/available?startDate=${startDate}&endDate=${endDate}`)
+            .then(response => {
+                setAvailableCars(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching available cars", error);
+               
+            });
+    };
+
+    return (
+
+      
+        <div>
+            <input 
+                type="date" 
+                value={startDate} 
+                onChange={e => setStartDate(e.target.value)} 
+            />
+            <input 
+                type="date" 
+                value={endDate} 
+                onChange={e => setEndDate(e.target.value)} 
+            />
+            <button onClick={fetchAvailableCars}>Buscar coches disponibles</button>
+            <div className="available-cars">
+              <h2></h2>
+        {
+          availableCars.map(car => (
+            <CardProduct key={car._id} car={car} />
+          ))
+        }
+      </div>
     </div>
   );
-};
+}
+
+
 
 export default FilterCar;
